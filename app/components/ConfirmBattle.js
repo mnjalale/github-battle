0746185616d585a39bf0c9c4ba31b671b47c5e8c
Martin Njalale <mnjalale@gmail.com>
2016-07-18 12:58:31 +0300

    Added Confirm Battle

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -2,3 +2,11 @@
  * Created by Martin on 18-Jul-16.
  */
 var React = require('react');
+
+function ConfirmBattle(props){
+    return props.isLoading===true
+        ? <p>Loading...</p>
+        : <p>Confirm Battle</p>
+}
+
+module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,9 +1,25 @@
 var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
 
 var ConfirmBattleContainer = React.createClass({
+    contextTypes:{
+        router: React.PropTypes.object.isRequired
+    },
+    getInitialState: function(){
+        return {
+            isLoading:true,
+            playerInfo:[]
+        }
+    },
+    componentDidMount:function(){
+        var query = this.props.location.query;
+        // Fetch infom from github then update state
+    },
     render: function(){
         return(
-            <div>Battle Container</div>
+            <ConfirmBattle
+                isLoading={this.state.isLoading}
+                playerInfo={this.state.playerInfo}/>
         )
     }
 });
